ffa259802db8b8736c8aa48495e46f2635c69f5f
JMJarvis1 <97367692+JMJarvis1@users.noreply.github.com>
2023-09-12 13:09:39 +0000

    pipenv, twilio integration, gitignore

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from logging import config
+from twilio.rest import Client
+import config 
+
+
+
+client = Client(config.account_sid, config.auth_token)
+
+call = client.messages.create(
+    to="+17342556699"
+    from_="+18556831598"
+    body="This is our first message"
+)
+
 
